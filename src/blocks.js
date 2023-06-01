ce876489f3b7d5d8ce0f5484a7cbe0312282dbe1
Ashton Drye <120841822+Askofep-1@users.noreply.github.com>
2023-06-01 19:38:52 -0400

    Update blocks.js

--- src/blocks.js
+++ src/blocks.js
@@ -885,6 +885,10 @@ SyntaxElementMorph.prototype.labelParts = {
         type: 'text entry',
         tags: 'monospace'
     },
+    '%codes': {
+        type: 'text entry',
+        tags: 'monospace static'
+    },
 
     /*
         type: 'boolean'
@@ -1286,7 +1290,7 @@ SyntaxElementMorph.prototype.labelParts = {
         dflt: ['key','value'],
         infix: ',',
         tags: 'static widget'
-    }
+    }- =n m
 };
 
 // SyntaxElementMorph instance creation:
